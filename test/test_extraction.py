2db47ed13f90af3a7c6816d7d4c32d9f89e786bc
Eduardo Gonzalez Lopez de Murillas <e.gonzalez@tue.nl>
2018-05-03 00:09:58 +0200

    Added option to extractor to use custom metadata Fixed bugs on extraction: _Extracting tables instead of classes _Using values of all columns as key when pk is not defined _When several unique constraints are defined for table, use first one as pk

--- test/test_extraction.py
+++ test/test_extraction.py
@@ -1,11 +1,10 @@
 import eddytools.extraction as ex
+import eddytools.schema as es
 import sqlalchemy as sq
+from sqlalchemy.sql.expression import text
 
 
-def test_ds2():
-    openslex_file_path = 'data/sample.slexmm'
-
-    connection_params = {
+connection_params = {
         'dialect': 'postgresql',
         'username': 'ds2',
         'password': 'ds2',
@@ -15,51 +14,54 @@ def test_ds2():
         'schema': 'public',
     }
 
+openslex_file_path = 'data/sample.slexmm'
+
+
+def test_ds2():
+
     classes = None
     try:
         ex.extract_to_mm(openslex_file_path, **connection_params,
                          classes=classes, overwrite=True)
-        mm_engine = ex.create_mm_engine(openslex_file_path)
-        mm_conn = mm_engine.connect()
-        mm_q = sq.select('*').select_from('object as O, class as CL').where('O.class_id == CL.id AND CL.name == "customers"')
-        mm_res = mm_conn.execute(mm_q).fetchall()
-        mm_conn.close()
-
-        db_engine = ex.create_db_engine(**connection_params)
-        db_conn = db_engine.connect()
-
-        db_q = db_q = sq.select('*').select_from('public.customers')
-        db_res = db_conn.execute(db_q).fetchall()
-        db_conn.close()
-
-        assert db_res.__len__() == mm_res.__len__()
-
+        assert check_mm(openslex_file_path, connection_params)
     except Exception as e:
         raise e
 
 
-def main():
-    openslex_file_path = 'data/sample.slexmm'
+def check_mm(openslex_file_path, connection_params):
+    mm_engine = ex.create_mm_engine(openslex_file_path)
+    mm_conn = mm_engine.connect()
+    mm_q = sq.select('*').select_from(text('object as O, class as CL')) \
+        .where(text('O.class_id == CL.id AND CL.name == "customers"'))
+    mm_res = mm_conn.execute(mm_q).fetchall()
+    mm_conn.close()
 
-    connection_params = {
-        'dialect': 'postgresql',
-        'username': 'postgres',
-        'password': 'postgres',
-        'host': 'localhost',
-        'port': '5555',
-        'database': 'mimic',
-        'schema': 'public',
-    }
+    db_engine = ex.create_db_engine(**connection_params)
+    db_conn = db_engine.connect()
 
-    classes = ['patients', 'admissions', 'microbiologyevents',
-              'd_items']  # use this to specify a subset of the classes
-    try:
-        ex.extract_to_mm(openslex_file_path, **connection_params,
-                         classes=classes, overwrite=True)
-    except Exception as e:
-        raise e
+    db_q = db_q = sq.select('*').select_from(text('public.customers'))
+    db_res = db_conn.execute(db_q).fetchall()
+    db_conn.close()
+
+    check = db_res.__len__() == mm_res.__len__()
+    assert check
+    print(check)
+    return check
+
+
+def test_custom_metadata_extraction():
+    db_engine = ex.create_db_engine(**connection_params)
+    schema = connection_params['schema']
+    discovered_pks = es.discover_pks(db_engine, schema=schema)
+    discovered_fks = es.discover_fks(db_engine, schema=schema,
+                                     pk_candidates=discovered_pks)
+    db_meta = es.create_custom_metadata(connection_params, db_engine,
+                                        connection_params['schema'],
+                                        discovered_pks, discovered_fks)
+    ex.extract_to_mm(openslex_file_path, **connection_params, overwrite=True, metadata=db_meta)
+    assert check_mm(openslex_file_path, connection_params)
 
 
 if __name__ == '__main__':
-    test_ds2()
-    #main()
+    #test_ds2()
+    test_custom_metadata_extraction()
