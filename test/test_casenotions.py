3aca2d340e749ce6c635c1553edf4f724d51c539
Eduardo Gonzalez Lopez de Murillas <e.gonzalez@tue.nl>
2018-05-08 15:13:27 +0200

    Added case notion stats computation

--- test/test_casenotions.py
+++ test/test_casenotions.py
@@ -1,6 +1,7 @@
 import eddytools.extraction as ex
 import eddytools.casenotions as cn
 import json
+import os
 
 openslex_file_path = 'data/mm/metamodel-sample.slexmm'
 
@@ -13,11 +14,15 @@ def test_candidates():
         assert r['rs'] in ['CONCERT_HALL_FK', 'SEAT_HALL_FK', 'BP_CONCERT_FK', 'BP_BAND_FK',
                            'TICKET_CONCERT', 'TICKET_SEAT', 'BOOKING_CON', 'BOOKING_FK']
 
+    stats = cn.get_stats_mm(mm_engine)
+
+    os.makedirs('data/dumps/')
+
+    json.dump(stats, open('data/dumps/stats_mm.json', 'wt'), indent=True)
+
     candidates = cn.compute_candidates(mm_engine)
 
-    json.dump(candidates, open('data/dumps/candidates.json', 'wt'))
-
-
+    json.dump(candidates, open('data/dumps/candidates.json', 'wt'), indent=True)
 
     assert candidates.__len__() > 0
 
