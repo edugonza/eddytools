ff6679e45b81dcf376b37da7843d5a0290a906f5
Eduardo Gonzalez Lopez de Murillas <e.gonzalez@tue.nl>
2018-05-10 15:42:29 +0200

    More lines of logs

--- data/ds2/check-health.sh
+++ data/ds2/check-health.sh
@@ -3,7 +3,7 @@
 HEALTH="no"
 
 while [ "$HEALTH" != "\"healthy\"" ]; do
-	docker logs --tail 100 ds2
+	docker logs --tail 200 ds2
 	sleep 5
 	HEALTH=`docker inspect --format="{{json .State.Health.Status}}" ds2`
 	echo "Health check for ds2: ${HEALTH}"
