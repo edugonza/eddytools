59c3f1120ad95499db5fed0042e5e8f109aeeff7
Eduardo Gonzalez Lopez de Murillas <e.gonzalez@tue.nl>
2018-05-10 16:07:01 +0200

    Removed volume from docker run command

--- data/ds2/run-image.sh
+++ data/ds2/run-image.sh
@@ -6,5 +6,5 @@ HOST_PGDATA=`pwd`'/postgresql'
 docker run --name ds2 -p $HOST_PORT:5432 \
 	-e BUILD_DATA=$BUILD_DATA \
 	-e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
-	-v $HOST_PGDATA:/var/lib/postgresql/data \
 	-d postgres/ds2
+#	-v $HOST_PGDATA:/var/lib/postgresql/data \
