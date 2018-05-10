d0f68793005071db1d470725c361e5a03d11c34a
Eduardo Gonzalez Lopez de Murillas <e.gonzalez@tue.nl>
2018-05-10 17:00:49 +0200

    Changed docker run script

--- data/ds2/run-image.sh
+++ data/ds2/run-image.sh
@@ -2,9 +2,10 @@
 BUILD_DATA=1
 HOST_PORT=5556
 POSTGRES_PASSWORD='postgres'
-HOST_PGDATA=`pwd`'/postgresql'
 docker run --name ds2 -p $HOST_PORT:5432 \
 	-e BUILD_DATA=$BUILD_DATA \
 	-e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
 	-d postgres/ds2
-#	-v $HOST_PGDATA:/var/lib/postgresql/data \
+sleep 4
+./check_health
+docker exec -u postgres ds2 /setup.sh
