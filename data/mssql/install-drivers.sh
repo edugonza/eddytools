0e59761e85111241769e714812ec13f033b0d659
Eduardo Gonzalez Lopez de Murillas <e.gonzalez@tue.nl>
2018-05-11 17:07:43 +0200

    Removed optional dependencies from mssql driver installation

--- data/mssql/install-drivers.sh
+++ data/mssql/install-drivers.sh
@@ -25,4 +25,4 @@ echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
 # optional: for unixODBC development headers
-sudo apt-get install unixodbc-dev
+#sudo apt-get install unixodbc-dev
