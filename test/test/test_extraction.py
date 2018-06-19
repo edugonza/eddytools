72ed6edf0a99b867da872fe4444552eefac59d7b
Eduardo Gonzalez Lopez de Murillas <e.gonzalez@tue.nl>
2018-06-19 18:40:17 +0200

    Reduced complexity of a test

--- test/test/test_extraction.py
+++ test/test/test_extraction.py
@@ -81,5 +81,5 @@ def test_custom_metadata_extraction():
 
 
 if __name__ == '__main__':
-    #test_ds2()
-    test_custom_metadata_extraction()
+    test_ds2()
+    #test_custom_metadata_extraction()
