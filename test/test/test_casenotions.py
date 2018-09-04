bcbc70f74ed8d96ec12f22e4e26f8419690dd3a4
Eduardo Gonzalez Lopez de Murillas <e.gonzalez@tue.nl>
2018-09-04 11:11:49 +0200

    fixed test case notions

--- test/test/test_casenotions.py
+++ test/test/test_casenotions.py
@@ -29,7 +29,7 @@ def test_candidates():
 
     random.shuffle(candidates)
 
-    for idx, cand in enumerate(candidates[:5]):
+    for idx, cand in enumerate(list(candidates.values())[:min(5, candidates.__len__())]):
         log_name = 'log_test_{}'.format(idx)
         print('Computing Log: {}'.format(log_name))
         cn.build_log_for_case_notion(mm_engine, cand,
